2cc37cd21df3028c7bcbbbc9c5965f2ea2a1f65c
sescandon <sescandon@unal.edu.co>
2024-04-14 11:54:38 -0500

    refactor: Replaced TextButtons with InkWells

--- lib/presentation/widgets/login/login_modal.dart
+++ lib/presentation/widgets/login/login_modal.dart
@@ -4,7 +4,6 @@ import "package:perfectpick_wa/data/repositories/auth/auth_repository.dart";
 import "package:perfectpick_wa/auxiliar_functions.dart";
 
 class LoginDialog extends StatefulWidget {
-  
   final AuthRepository authRepository;
 
   const LoginDialog({Key? key, required this.authRepository}) : super(key: key);
@@ -18,7 +17,6 @@ class LoginDialogState extends State<LoginDialog> {
   double _passwordSpacing = 10;
   final loginEmailDataController = TextEditingController();
   final loginPasswordDataController = TextEditingController();
-
 
   @override
   void dispose() {
@@ -43,7 +41,9 @@ class LoginDialogState extends State<LoginDialog> {
         ),
         child: Column(
           children: [
-            SizedBox(height: 12), // Added padding to the top of the dialog (16 pixels
+            SizedBox(
+                height:
+                    12), // Added padding to the top of the dialog (16 pixels
             Padding(
               padding: EdgeInsets.all(16),
               child: Image.asset(
@@ -59,7 +59,7 @@ class LoginDialogState extends State<LoginDialog> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      SizedBox(height: 12), 
+                      SizedBox(height: 12),
                       Text('Hi!',
                           textAlign: TextAlign.left,
                           style: TextStyle(
@@ -120,17 +120,20 @@ class LoginDialogState extends State<LoginDialog> {
                           ),
                         ),
                       ),
-                      TextButton(
-                        onPressed: () {
+                      InkWell(
+                        onTap: () {
                           // Handle forgot password action
                         },
-                        child: Text(
-                          'Forgot your password?',
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                              color: activeColor,
-                              decoration: TextDecoration.underline,
-                              decorationColor: activeColor),
+                        child: Padding(
+                          padding: EdgeInsets.only(top: 8),
+                          child: Text(
+                            'Forgot your password?',
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                                color: activeColor,
+                                decoration: TextDecoration.underline,
+                                decorationColor: activeColor),
+                          ),
                         ),
                       ),
                       SizedBox(
@@ -145,36 +148,40 @@ class LoginDialogState extends State<LoginDialog> {
                             onPressed: () async {
                               // Handle login action
                               String email = loginEmailDataController.text;
-                              String password = loginPasswordDataController.text;
+                              String password =
+                                  loginPasswordDataController.text;
                               try {
-                                String token = await widget.authRepository.emailLogin(email, password);
-                                showDialog(context: context, builder: (context) => AlertDialog(
-                                  title: Text('Success'),
-                                  content: Text('Token: $token'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('Close'),
-                                    ),
-                                  ],
-                                )
-                                );
+                                String token = await widget.authRepository
+                                    .emailLogin(email, password);
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                          title: Text('Success'),
+                                          content: Text('Token: $token'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text('Close'),
+                                            ),
+                                          ],
+                                        ));
                               } catch (e) {
-                                showDialog(context: context, builder: (context) => AlertDialog(
-                                  title: Text('Error'),
-                                  content: Text('Error: $e'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('Close'),
-                                    ),
-                                  ],
-                                )
-                                );
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                          title: Text('Error'),
+                                          content: Text('Error: $e'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text('Close'),
+                                            ),
+                                          ],
+                                        ));
                               }
                             },
                             style: ElevatedButton.styleFrom(
@@ -200,29 +207,30 @@ class LoginDialogState extends State<LoginDialog> {
                           child: TextButton(
                             onPressed: () async {
                               try {
-                                String link = await widget.authRepository.googleLogin();
+                                String link =
+                                    await widget.authRepository.googleLogin();
                                 try {
                                   launchAuxiliarURL(link);
                                 } catch (e) {
                                   print(e);
                                 }
                               } catch (e) {
-                                showDialog(context: context, builder: (context) => AlertDialog(
-                                  title: Text('Error'),
-                                  content: Text('Error: $e'),
-                                  actions: [
-                                    TextButton(
-                                      onPressed: () {
-                                        Navigator.of(context).pop();
-                                      },
-                                      child: Text('Close'),
-                                    ),
-                                  ],
-                                )
-                                );
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                          title: Text('Error'),
+                                          content: Text('Error: $e'),
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: Text('Close'),
+                                            ),
+                                          ],
+                                        ));
                                 print(e);
                               }
-                              
                             },
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
@@ -249,24 +257,28 @@ class LoginDialogState extends State<LoginDialog> {
                         height: Checkbox.width * 3,
                       ),
                       Align(
-                        alignment: Alignment.centerLeft,
-                        child: TextButton(
-                            onPressed: () {},
-                            child: Wrap(
-                              children: [
-                                Text(
-                                  'Don\'t have an account? ',
-                                  style: TextStyle(color: alternativeTextColor),
-                                  overflow: TextOverflow.fade,
+                          alignment: Alignment.centerLeft,
+                          child: Wrap(
+                            children: [
+                              Text(
+                                'Don\'t have an account? ',
+                                style: TextStyle(color: alternativeTextColor),
+                                overflow: TextOverflow.fade,
+                              ),
+                              InkWell(
+                                onTap: () => {
+                                  // Handle register action
+                                },
+                                child: Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                      color: activeColor,
+                                      decoration: TextDecoration.underline,
+                                      decorationColor: activeColor),
                                 ),
-                                Text('Sign Up',
-                                    style: TextStyle(
-                                        color: activeColor,
-                                        decoration: TextDecoration.underline,
-                                        decorationColor: activeColor)),
-                              ],
-                            )),
-                      )
+                              ),
+                            ],
+                          ))
                     ],
                   ),
                 ),
