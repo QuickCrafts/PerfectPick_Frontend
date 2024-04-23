7d11f505f479a8071684235fa06db78420d0eda2
sescandon <sescandon@unal.edu.co>
2024-04-22 23:46:32 -0500

    feat: ads container made

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:perfectpick_wa/presentation/pages/ads_landing.dart';
 import 'package:perfectpick_wa/presentation/widgets/cards/card_media.dart';
 import 'presentation/widgets/navigation/navbar.dart';
 import 'presentation/widgets/navigation/how_it_works.dart';
@@ -24,7 +25,8 @@ Future main() async {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AuthRepository mainAuthRepository = AuthRepository(client: graphqlClient.value);
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -36,10 +38,10 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
         ),
+        initialRoute: '/',
         routes: {
-          '/': (context) => MyHomePage(
-                authRepository: AuthRepository(client: graphqlClient.value),
-              ),
+          '/': (context) => MyHomePage(authRepository: mainAuthRepository),
+          '/ads': (context) => AdsLandingPage(authRepository: mainAuthRepository),
         },
       ),
     );
