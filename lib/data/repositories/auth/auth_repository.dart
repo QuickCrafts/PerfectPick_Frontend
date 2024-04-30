e88d3d4164efcb539807da7a1f6d7866d017858c
NicolasLopez147 <nilopezn@unal.edu.co>
2024-04-30 16:24:49 -0500

    Meging

--- lib/data/repositories/auth/auth_repository.dart
+++ lib/data/repositories/auth/auth_repository.dart
@@ -78,6 +78,9 @@ class AuthRepository {
     // Check result data
     final data = result.data!['loginWithGoogle'];
     potentialLink = data['url'];
+    if (potentialLink == null) {
+      throw Exception('No link received from the GraphQL query.');
+    }
     final String link = data['url'];
 
     return link;
@@ -201,6 +204,9 @@ class AuthRepository {
     // Check result data
     final data = result.data!['loginWithGoogle'];
     potentialLink = data['url'];
+    if (potentialLink == null) {
+      throw Exception('No link received from the GraphQL query.');
+    }
     final String link = data['url'];
 
     return link;
@@ -254,48 +260,6 @@ class AuthRepository {
     return outcomingModel.id;
   }
 
-  Future<List<BooksModel>> getBooks() async {
-    final List<BooksModel>? potentialList;
-
-    final QueryOptions options = getBooksQueryQueryOptions();
-    final QueryResult result = await client.query(options);
-
-    if (result.hasException) {
-      // Handle the exception
-      print('GraphQL exception: ${result.exception.toString()}');
-    } else if (result.data == null) {
-      // Handle the case when result.data is null
-      print('No data received from the GraphQL query.');
-    } else {
-      final data = result.data!['GetBooks'];
-      if (data != null && data is Map<String, dynamic>) {
-      } else {
-        print('Invalid data format received from the GraphQL query.');
-      }
-    }
-
-    // Check result data
-    final data = result.data!['GetBooks'];
-    final response = jsonDecode(data['books']);
-    potentialList = response['books'];
-    if (potentialList == null) {
-      final potentialList = response['books'];
-      if (potentialList != null) {
-        throw Exception(potentialList);
-      } else {
-        throw Exception('Unknown error occurred.');
-      }
-    }
-
-    BooksResponseModel outcomingModel =
-        BooksResponseModel(books: potentialList);
-    if (!outcomingModel.validate()) {
-      throw Exception('Invalid output');
-    }
-
-    return outcomingModel.books;
-  }
-
   Future<int> verifyID(String token) async {
     final int? potentialId;
 
@@ -313,6 +277,9 @@ class AuthRepository {
     print(result);
     potentialId = result.data!['id'];
     print(potentialId);
+    if (potentialId == null) {
+      throw Exception('No ID received from the GraphQL query.');
+    }
     return potentialId;
   }
 }
